33557e7d77ed7bce87689fbd7b8cc7cee9296297
joe <>
2024-04-29 23:42:21 +0800

    add meta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +10,12 @@ import { RouterOutlet } from '@angular/router';
     <router-outlet />
   `
 })
-export class AppComponent {}
+export class AppComponent {
+  meta =  inject(Meta);
+
+  constructor() {
+    this.meta.addTag({ name: 'description', content: 'Angular TodoMVC use Signal + SSR + PWA'});
+    this.meta.addTag({ name: 'author', content: 'Joe' });
+    this.meta.addTag({ name: 'keywords', content: 'Angular, TodoMVC, Signal, SSR, PWA' }); 
+  }
+}
