ef22111630372248df3ce84af80a048fc950632f
joe <>
2024-04-29 21:14:41 +0800

    add PWA

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(withFetch()),
-    provideRouter(
-      routes, 
-      withHashLocation(),
-      withComponentInputBinding(),
-    ), provideClientHydration()
-  ]
+    provideRouter(routes, withHashLocation(), withComponentInputBinding()), provideClientHydration(),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 };
